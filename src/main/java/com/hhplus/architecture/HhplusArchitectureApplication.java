7164aaaf7d8d7dc93cea731a10106672e78f2b24
taek00n <kimth0526@naver.com>
2024-09-29 23:07:32 +0900

    first commit

---
+++ src/main/java/com/hhplus/architecture/HhplusArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhplus/architecture/HhplusArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
